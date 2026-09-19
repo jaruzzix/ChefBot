309893012a33341e50170891f032d2a255612c1a
jaruzzix <vederni.kov@mail.ru>
2026-09-19 19:59:35 +0300

    Поправил инлайн клавиатуры списков

--- utils/keyboards/inline/saves_list.py
+++ utils/keyboards/inline/saves_list.py
@@ -28,14 +28,15 @@ def saves_list_ikb(items: Dict[str, Any], page_max_length: int | None = None, pa
         builder.row(InlineKeyboardButton(text=items[key]["title"], callback_data=key))
 
     if page_max_length:
-        if page == 0 and page < max_page:
-            builder.row(InlineKeyboardButton(text="След. страница", callback_data=f"page_{page + 1}"))
-        elif page == max_page and max_page != 0:
-            builder.row(InlineKeyboardButton(text="Пред. страница", callback_data=f"page_{page - 1}"))
-        else:
-            builder.row(
-                InlineKeyboardButton(text="Пред. страница", callback_data=f"page_{page - 1}"),
-                InlineKeyboardButton(text="След. страница", callback_data=f"page_{page + 1}")
+        if max_page:
+            if page == 0 and page < max_page:
+                builder.row(InlineKeyboardButton(text="След. страница", callback_data=f"page_{page + 1}"))
+            elif page == max_page and max_page != 0:
+                builder.row(InlineKeyboardButton(text="Пред. страница", callback_data=f"page_{page - 1}"))
+            else:
+                builder.row(
+                    InlineKeyboardButton(text="Пред. страница", callback_data=f"page_{page - 1}"),
+                    InlineKeyboardButton(text="След. страница", callback_data=f"page_{page + 1}")
             )
 
 
